e3d712295fe5997c52773dc5d9ec4e908e284cfc
Stig Petersen <stigpetersen4@gmail.com>
2022-10-10 20:01:29 +0200

    Initial commit

---
+++ web/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {AppBar, Toolbar, Typography} from "@mui/material";
+import {giraffeStore} from "./stores/GiraffeStore";
+
+function App() {
+  return (
+    <div>
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/giraffe" element={<Giraffes/>}/>
+        </Routes>
+    </div>
+  );
+}
+
+ const About = () => {
+     return <div>
+         <h1>Hi</h1>
+     </div>
+ }
+
+ const Home= () => {
+        return <div>
+            <h1>Home</h1>
+        </div>
+ }
+
+const Giraffes= () => {
+    return <div>
+        <Typography>Girafliste</Typography>
+        <ul>
+            {giraffeStore.giraffes.map((giraffeName,key)=>
+                <li key={key}>{giraffeName}</li>
+            )}
+        </ul>
+    </div>
+}
+ const Header = () => {
+     return (
+         <AppBar position="static">
+             <Toolbar>
+                 <Typography variant="h4">
+                     Navbar
+                 </Typography>
+             </Toolbar>
+         </AppBar>
+     )
+}
+
+export default App;
